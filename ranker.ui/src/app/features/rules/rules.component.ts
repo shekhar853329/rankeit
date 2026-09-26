94dedc3edfaf444c2840c6ae40f9017495977524
sumanpriya15 <priyasuman15@hotmail.com>
2026-09-26 21:34:25 +0530

    Rules Update

--- ranker.ui/src/app/features/rules/rules.component.ts
+++ ranker.ui/src/app/features/rules/rules.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-rules',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './rules.component.html',
   styleUrl: './rules.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RulesComponent {}
+
